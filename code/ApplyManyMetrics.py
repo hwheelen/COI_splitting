111fbc046a9ce003add9459f43519d80ea888e96
hwheelen <hwheelen@berkeley.edu>
2021-02-19 13:51:24 -0500

    updated code

--- code/ApplyManyMetrics.py
+++ code/ApplyManyMetrics.py
@@ -2,33 +2,27 @@ import geopandas as gpd
 from shapely.geometry import Polygon
 import numpy as np
 
-grid = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/RichmondGrid.shp')
 
-boundary = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/Boundary.shp')
+#load in communities
+richmond = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Voter_Precincts_RichmondVA/RichmondCOIs.shp')
 
-half = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/half.shp')
-
-split_25_75 = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/split_25_75.shp')
-
-split_10_90 = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/split_10_90.shp')
-
-quarters = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/split_quarters.shp')
-
-four_uneven = gpd.read_file('/Users/hwheelen/Documents/GitHub/COI_Splitting/Hypothetical Data/splits/split_four_uneven.shp')
-
-#make list of types of splits we want to calculate
-splits = [boundary, half, split_25_75, split_10_90, quarters, four_uneven]
+#calculate population of each community and add it to a new column
 
 
-#first calculate simple splits aka how many times the community is cut
+#load in maps
+map1 = gpd.read_file('/Users/hwheelen/Documents/GitHub/VA-gerrymander/Maps/Special Master Map/Court Order Map/Shapefile/New_Map.shp)
+
+map2 = gpd.read_file('/Users/hwheelen/Documents/GitHub/VA-gerrymander/Maps/Enacted map/enacted.shp')
+
+map3 = gpd.read_file('/Users/hwheelen/Documents/GitHub/VA-gerrymander/Maps/Reform map/Reform map all districts.shp')
 
 
-#then calculate MGGG split edges 
+#first, identify which maps split which communities
 
 
-#calculate Jacobs splitting
+#next, calculate how many components each community is split into
 
-#calculate effective number of components, or Taagepera components
+#next, calculate Laakso and Taagepera number of compoonents of each community
 for split in splits:
     #aggregate population into each component
     #define each component of split
@@ -38,3 +32,10 @@ for split in splits:
         comp_prop_pop = 
     #add up squares of each prop
     #save 1/(sum of squares) as effective number of components
+
+#then calculate MGGG split edges 
+
+
+#calculate Jacobs splitting
+
+#calculate effective number of components, or Taagepera components
